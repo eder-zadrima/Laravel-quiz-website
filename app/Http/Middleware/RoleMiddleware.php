5bd31781baaf2226b14a76fe575e4b70cee634ab
yuriihorodnyi21 <yuriihorond450@gmail.com>
2021-04-29 18:24:56 +0900

    fix rolemiddleware

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -16,6 +16,9 @@ class RoleMiddleware
      */
     public function handle($request, Closure $next, $role, $permission = null)
     {
+        if($request->user() == null) {
+            abort(404);
+        };
         if(!$request->user()->hasRole($role)) {
 
              abort(404);
