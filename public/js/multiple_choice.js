413ed9793c1064aa63db8f28d3d739f1843ee9a4
yuriihorodnyi21 <yuriihorond450@gmail.com>
2021-05-14 00:50:39 +0900

    fix multiple_choice & multiple_response js

--- public/js/multiple_choice.js
+++ public/js/multiple_choice.js
@@ -1,5 +1,7 @@
 function get_choice_item_id() {
-    const length = $('.choice_item').length;
+    const length = $('#choice_list .choice_item').length;
+
+    console.log(length);
 
     let id = 1;
 
@@ -7,8 +9,8 @@ function get_choice_item_id() {
         id = 0;
 
         for (let i = 0; i < length; i++) {
-            const inputId = parseInt($('.choice_item input').eq(i).attr('id'));
-            if (inputId > id) id = inputId + 1;
+            const inputId = parseInt($('#choice_list .choice_item input').eq(i).attr('value'));
+            if (inputId >= id) id = inputId + 1;
         }
     }
 
@@ -19,6 +21,6 @@ $('#add_choice').click(function () {
 
     const id = get_choice_item_id();
 
-    const element = $('<tr class="choice_item"><td><input type="radio" id="' + id + '" name="answer" value="' + id + '" style="padding-right: 10px;"></td><td><label class="choice_label" data-editable for="' + id + '">Type content ...</label></td><td></td><td><a onclick="{$(this).parent().parent().remove();}"><i class="fas fa-trash-alt"></i></a></td></tr>');
+    const element = $('<tr class="choice_item"><td><input type="radio" name="answer" value="' + id + '" style="padding-right: 10px;"></td><td><label class="choice_label" data-editable for="' + id + '">Type content ...</label></td><td></td><td><a onclick="{$(this).parent().parent().remove();}"><i class="fas fa-trash-alt"></i></a></td></tr>');
     $('tbody#choice_list').append(element);
 });
--- public/js/multiple_response.js
+++ public/js/multiple_response.js
@@ -1,5 +1,7 @@
 function get_response_item_id() {
-    const length = $('.response_item').length;
+    const length = $('#response_list .response_item').length;
+
+    console.log(length);
 
     let id = 1;
 
@@ -7,8 +9,10 @@ function get_response_item_id() {
         id = 0;
 
         for (let i = 0; i < length; i++) {
-            const inputId = parseInt($('.response_item input').eq(i).attr('id'));
-            if (inputId > id) id = inputId + 1;
+            const inputId = parseInt($('#response_list .response_item input').eq(i).attr('value'));
+            console.log(inputId);
+            if (inputId >= id) id = inputId + 1;
+            console.log(id);
         }
     }
 
@@ -19,6 +23,6 @@ $('#add_response').click(function () {
 
     const id = get_response_item_id();
 
-    const element = $('<tr class="response_item"><td><input type="checkbox" id="' + id + '" name="answer" value="' + id + '" style="padding-right: 10px;"></td><td><label class="response_label" data-editable for="' + id + '">Type content ...</label></td><td></td><td><a onclick="{$(this).parent().parent().remove();}"><i class="fas fa-trash-alt"></i></a></td></tr>');
+    const element = $('<tr class="response_item"><td><input type="checkbox" name="answer" value="' + id + '" style="padding-right: 10px;"></td><td><label class="response_label" data-editable for="' + id + '">Type content ...</label></td><td></td><td><a onclick="{$(this).parent().parent().remove();}"><i class="fas fa-trash-alt"></i></a></td></tr>');
     $('tbody#response_list').append(element);
 });
