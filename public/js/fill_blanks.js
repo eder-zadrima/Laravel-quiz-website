d73d83305d89de758599471b139a0aeba49cbcdd
yuriihorodnyi21 <yuriihorond450@gmail.com>
2021-06-09 11:09:06 +0900

    update slide view nav

--- public/js/fill_blanks.js
+++ public/js/fill_blanks.js
@@ -1,7 +1,7 @@
 function add_word(element) {
-    element.parent().parent().prepend('<li><label data-editable>Click and Type content...</label><a onclick="{$(this).parent().remove();update_slide_view_nav();}"><i class="fas fa-trash-alt"></i></a></li>');
+    element.parent().parent().prepend('<li><label data-editable>Click and Type content...</label><a onclick="{$(this).parent().remove();localStorage.setItem(\'is_edited\', \'true\');}"><i class="fas fa-trash-alt"></i></a></li>');
 
-    update_slide_view_nav();
+    localStorage.setItem('is_edited', 'true');
 }
 
 function add_fill_word_dropdown() {
@@ -13,7 +13,7 @@ function add_fill_word_dropdown() {
     }
     $('#fill_blanks').append('<div class="fill_blanks_dropdown" contenteditable="true" style="display: inline;"><div class="fill_blanks_dropdown_body" contenteditable="false"  style="display: inline;"><div class="fill_blanks_dropdown_content"></div><div class="fill_blanks_dropdown_arrow" onclick="toggle_fill_blanks_dropdown(this)"><i class="fas fa-chevron-down"></i></div><div class="fill_blanks_dropdown_menu" contenteditable="false"><ul><li><i onclick="add_word($(this));">Add a new word</i></li></ul></div></div></div>&nbsp');
 
-    update_slide_view_nav();
+    localStorage.setItem('is_edited', 'true');
 }
 
 function toggle_fill_blanks_dropdown(element) {
