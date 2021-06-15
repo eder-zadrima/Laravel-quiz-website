0ba5c8abd346a642952f7dfbd6c312fc1831d387
yuriihorodnyi21 <yuriihorond450@gmail.com>
2021-06-15 14:50:04 +0900

    fix email

--- resources/views/emails/quizResultMail.blade.php
+++ resources/views/emails/quizResultMail.blade.php
@@ -123,11 +123,11 @@
                                             Correct Answer
                                         </td>
                                     </tr>
-                                    @for ($i = 0; $i < count($question['question_user_answer']); $i++)
+                                    @for ($i = 0; $i < count($question['question_correct_answer']); $i++)
 
                                     <tr>
                                         <td style="border: 1px solid #E0E0E0;padding:5px">
-                                            {{ $question['question_user_answer'][$i] }}
+                                            {{ $question['question_user_answer'][$i] ?? '' }}
                                         </td>
                                         <td style="border: 1px solid #E0E0E0;padding:5px">
                                             {{ $question['question_correct_answer'][$i] }}
