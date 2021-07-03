c9ffeaaed8207716090ff9fd82d42e8c3b4b8ef5
yuriihorodnyi21 <yuriihorond450@gmail.com>
2021-07-03 12:12:03 +0900

    build get quiz html api

--- app/Http/Controllers/API/ExamController.php
+++ app/Http/Controllers/API/ExamController.php
@@ -1,15 +1,18 @@
 <?php
+
 namespace App\Http\Controllers\API;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\API\BaseController as BaseController;
 use App\Models\Exam;
 use Validator;
+
 //use App\Http\Resources\Product as ProductResource;
 
 class ExamController extends BaseController
 {
-    public function get_all_index() {
+    public function get_all_index()
+    {
         $quizzes = Exam::all();
 
         $quizzes_index = [];
@@ -17,12 +20,13 @@ class ExamController extends BaseController
             array_push($quizzes_index, $quiz->id);
         }
 
-        $success['data'] =  $quizzes_index;
+        $success['data'] = $quizzes_index;
 
         return $this->sendResponse($success, 'success');
     }
 
-    public function get_downloading_quizzes_index() {
+    public function get_downloading_quizzes_index()
+    {
         $quizzes = Exam::where('downloaded', 0)->get();
 
         $quizzes_index = [];
@@ -30,34 +34,36 @@ class ExamController extends BaseController
             array_push($quizzes_index, $quiz->id);
         }
 
-        $success['data'] =  $quizzes_index;
+        $success['data'] = $quizzes_index;
 
         return $this->sendResponse($success, 'success');
     }
 
-    public function get_quiz($id) {
+    public function get_quiz($id)
+    {
         $quiz = Exam::find($id);
 
         $success['data'] = $quiz;
         return $this->sendResponse($success, 'success');
     }
 
-    public function get_quiz_assets_url($id) {
+    public function get_quiz_assets_url($id)
+    {
         $url_list = [];
 
         $quiz = Exam::find($id);
 
-        if(isset($quiz->theme_style)) array_push($url_list, explode('"); background-size:' , explode('background-image:url("', $quiz->theme_style)[1])[0]);
+        if (isset($quiz->theme_style)) array_push($url_list, explode('"); background-size:', explode('background-image:url("', $quiz->theme_style)[1])[0]);
 
         $questions = $quiz->get_all_questions();
 
         foreach ($questions as $question) {
-            if(isset($question[0]->media)) array_push($url_list, $question[0]->media);
-            if(isset($question[0]->video)) array_push($url_list, $question[0]->video);
-            if(isset($question[0]->audio)) array_push($url_list, $question[0]->audio);
-            if(isset($question[0]->background_img)) array_push($url_list, explode('")', explode('url("', $question[0]->background_img)[1])[0]);
+            if (isset($question[0]->media)) array_push($url_list, $question[0]->media);
+            if (isset($question[0]->video)) array_push($url_list, $question[0]->video);
+            if (isset($question[0]->audio)) array_push($url_list, $question[0]->audio);
+            if (isset($question[0]->background_img)) array_push($url_list, explode('")', explode('url("', $question[0]->background_img)[1])[0]);
 
-            if(isset($question[0]->other_elements)) {
+            if (isset($question[0]->other_elements)) {
                 $tmp_array = explode('src="', $question[0]->other_elements);
                 array_shift($tmp_array);
 
@@ -69,5 +75,32 @@ class ExamController extends BaseController
 
         $success['data'] = $url_list;
         return $this->sendResponse($success, 'success');
+    }
+
+    public function get_quiz_html(string $id)
+    {
+//        if (isset(Auth::user()->id)) {
+//            $user = Auth::user();
+//            $name = $user->name;
+//            $email = $user->email;
+//        }
+//        if (session('name')) {
+//            $name = session('name');
+//            $email = session('email');
+//        }
+
+        $exams = Exam::where('id', $id)->get();
+        $title = $exams[0]->name;
+        $is_quiz = 1;
+
+        $exam_groups = $exams[0]->exam_groups;
+        $quizzes = [];
+        foreach ($exam_groups as $exam_group) {
+            foreach ($exam_group->quizes as $quiz) {
+                array_push($quizzes, $quiz);
+            }
+        }
+
+        return view('preview', ['quizzes' => $quizzes, 'title' => $title, 'is_quiz' => $is_quiz]);
     }
 }
